f465f2d8a63979045c00b9f3c9d6d810860af589
Jan Losinski <losinski@wh2.tu-dresden.de>
2017-05-18 18:12:12 +0200

    Add abilityto log to file too
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- syslog_receiver.py
+++ syslog_receiver.py
@@ -2,6 +2,7 @@
 import socketserver
 import datetime
 import argparse
+import logging
 
 
 class SyslogUDPHandler(socketserver.BaseRequestHandler):
@@ -10,14 +11,23 @@ class SyslogUDPHandler(socketserver.BaseRequestHandler):
         socket = self.request[1]
         print(datetime.datetime.now().strftime("%Y-%d-%m - %H:%M:%S"), "{} : ".format(self.client_address[0]),
               str(data))
+        if log_file:
+            logging.info(str(data))
 
 
 parser = argparse.ArgumentParser(description="Simple syslog message receiver")
 parser.add_argument("--host", help="Hostname to listen on", default="0.0.0.0")
 parser.add_argument("--port", help="Port to listen on", default=6656)
+parser.add_argument("--file", help="also log to file")
 
 if __name__ == "__main__":
     args = parser.parse_args()
+
+    if "file" in args and args.file:
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%Y-%d-%m - %H:%M:%S', filename=args.file, filemode='a')
+        log_file = True
+    else:
+        log_file = False
 
     try:
         server = socketserver.UDPServer((args.host, args.port), SyslogUDPHandler)
