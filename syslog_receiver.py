07bd0b74c3c26f15eb34561ea66d9b4dafa780c1
Jan Losinski <losinski@wh2.tu-dresden.de>
2017-05-18 03:23:11 +0200

    Add simple syslog receiver
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

---
+++ syslog_receiver.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import socketserver
+import argparse
+
+
+class SyslogUDPHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        data = bytes.decode(self.request[0].strip())
+        socket = self.request[1]
+        print("%s : " % self.client_address[0], str(data))
+
+
+parser = argparse.ArgumentParser(description="Simple syslog message receiver")
+parser.add_argument("--host", help="Hostname to listen on", default="0.0.0.0")
+parser.add_argument("--port", help="Port to listen on", default=6656)
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    try:
+        server = socketserver.UDPServer((args.host, args.port), SyslogUDPHandler)
+        server.serve_forever(poll_interval=0.5)
+    except (IOError, SystemExit):
+        raise
+    except KeyboardInterrupt:
+        print("Crtl+C Pressed. Shutting down.")
