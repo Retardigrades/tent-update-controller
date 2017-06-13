6f3dc85e562ebdd594ce226535647d8c2fce267a
Jan Losinski <losinski@wh2.tu-dresden.de>
2017-06-13 04:24:29 +0200

    Reformat
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- lirc_receiver.py
+++ lirc_receiver.py
@@ -1,29 +1,28 @@
 #!/usr/bin/env python3
- 
+
 import lirc
 import time
 import requests
- 
-sockid=lirc.init("tent-ir", blocking = False)
- 
+
+sockid = lirc.init("tent-ir", blocking=False)
+
 while True:
     codeIR = lirc.nextcode()
     if not codeIR:
-      continue
+        continue
     codeIR = codeIR[0]
 
     try:
         if codeIR == "vol-up":
-          r = requests.get('http://localhost:8080/music/volup')
+            r = requests.get('http://localhost:8080/music/volup')
         elif codeIR == "vol-down":
-          r = requests.get('http://localhost:8080/music/voldown')
+            r = requests.get('http://localhost:8080/music/voldown')
         elif codeIR == "next-song":
-          r = requests.get('http://localhost:8080/music/next')
+            r = requests.get('http://localhost:8080/music/next')
         elif codeIR == "next-effect":
-          print("Not implemented yet: " + codeIR)
+            print("Not implemented yet: " + codeIR)
         elif codeIR != []:
-          print(codeIR[0])
+            print(codeIR[0])
         time.sleep(0.05)
     except Exception as e:
         print(e)
-
