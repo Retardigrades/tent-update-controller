b156ff4b86f8c25a45380f2f3476bd58922fed0a
Jan Losinski <losinski@wh2.tu-dresden.de>
2017-06-13 05:04:44 +0200

    Rework lirc handler
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- lirc_receiver.py
+++ lirc_receiver.py
@@ -1,28 +1,65 @@
 #!/usr/bin/env python3
 
 import lirc
-import time
 import requests
+import argparse
 
-sockid = lirc.init("tent-ir", blocking=False)
 
-while True:
-    codeIR = lirc.nextcode()
-    if not codeIR:
-        continue
-    codeIR = codeIR[0]
+class Receiver(object):
+    def __init__(self, proc_name, base_url, url_map):
+        self.sockid = lirc.init(proc_name, blocking=False)
+        self.base_url = base_url
+        self.url_map = url_map
 
-    try:
-        if codeIR == "vol-up":
-            r = requests.get('http://localhost:8080/music/volup')
-        elif codeIR == "vol-down":
-            r = requests.get('http://localhost:8080/music/voldown')
-        elif codeIR == "next-song":
-            r = requests.get('http://localhost:8080/music/next')
-        elif codeIR == "next-effect":
-            print("Not implemented yet: " + codeIR)
-        elif codeIR != []:
-            print(codeIR[0])
-        time.sleep(0.05)
-    except Exception as e:
-        print(e)
+    def run(self):
+        while True:
+            codeIR = lirc.nextcode()
+
+            if not codeIR:
+                continue
+
+            command = codeIR[0]
+
+            if command not in self.url_map:
+                print("Unknown code: {}".format(command))
+                continue
+
+            url = self.url_map[command]
+
+            full_url = '{}{}'.format(self.base_url, url)
+            try:
+                response = requests.get(full_url)
+            except IOError as e:
+                print("Could not connect to {}: {}".format(full_url, e))
+
+
+def parse_command(inp_cmd):
+    cmd, url = inp_cmd.split(":")
+    return cmd, url
+
+
+parser = argparse.ArgumentParser(description="Lirc receiver")
+parser.add_argument("--name", help="Program name to lircd", default="tent-ir")
+parser.add_argument("--base", help="Set base url", required=True)
+parser.add_argument("commands", help="The command-url mappings in the form command:url", nargs="+", type=parse_command)
+
+default_commands = {
+    "vol-up": '/music/volup',
+    "vol-down": '/music/voldown',
+    "next-song": '/music/next'
+}
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    url_map = args.commands
+
+    if not url_map:
+        print("use default map")
+        url_map = default_commands
+    else:
+        url_map = dict(url_map)
+
+    handler = Receiver(args.name, args.base, url_map)
+
+    handler.run()
